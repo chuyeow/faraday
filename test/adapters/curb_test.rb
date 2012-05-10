964ba2524ef008f980cedcbb6d9c81483383b71b
Cheah Chu Yeow <chuyeow@gmail.com>
2012-05-10 22:44:54 +0800

    Turns out that Curb does support GET requests with body when using Curl::Easy#http(:GET).

--- test/adapters/curb_test.rb
+++ test/adapters/curb_test.rb
@@ -6,9 +6,6 @@ module Adapters
     def adapter() :curb end
 
     Integration.apply(self, :NonParallel) do
-      # Curb doesn't support GET requests with body.
-      undef :test_GET_with_body
-
       def test_binds_local_socket
         host = '1.2.3.4'
         conn = create_connection :request => { :bind => { :host => host } }
